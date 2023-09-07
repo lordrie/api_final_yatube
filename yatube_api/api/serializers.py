c0757dfdd76c4d51a13b7d893fdd1f4a7bec4516
Никита Прокофьев <aeriexye@gmail.com>
2023-09-07 16:48:15 +0300

    create serializers

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,23 +1,44 @@
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
-
-
-from posts.models import Comment, Post
+from posts.models import Comment, Post, Group, Follow, User
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
+    """Сериализатор для модели Post."""
+    author = serializers.SlugRelatedField(read_only=True,
+                                          slug_field='username')
+    comments = serializers.StringRelatedField(many=True,
+                                              required=False)
 
     class Meta:
-        fields = '__all__'
         model = Post
+        fields = ('id', 'author', 'text',
+                  'pub_date', 'image', 'group')
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
-    )
+    author = serializers.SlugRelatedField(slug_field='username')
 
     class Meta:
-        fields = '__all__'
         model = Comment
+        fields = ('id', 'text', 'created', 'author', 'post')
+        read_only_fields = ('post', 'author')
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Group."""
+    class Meta:
+        model = Group
+        fields = ('id', 'title', 'slug', 'description')
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Follow."""
+    user = serializers.SlugRelatedField(
+        slug_field='username', read_only=True,
+        default=serializers.CurrentUserDefault())
+    following = serializers.SlugRelatedField(
+        slug_field='username', queryset=User.objects.all())
+
+    class Meta:
+        model = Follow
+        fields = ('id', 'user', 'following')
