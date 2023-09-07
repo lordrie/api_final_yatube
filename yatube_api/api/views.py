8bebbe65a54182205a52b15efccf92c03b1c7d6b
Никита Прокофьев <aeriexye@gmail.com>
2023-09-07 19:46:39 +0300

    create permissions

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,38 @@
-# TODO:  Напишите свой вариант
+from posts.models import Post, Group
+from .serializers import PostSerializer, GroupSerializer, CommentSerializer
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from django.shortcuts import get_object_or_404
+from .permissions import OwnerOrReadOnly, GroupOnlyGet
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    """Вьюсет для работы с моделью Post."""
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (OwnerOrReadOnly, IsAuthenticatedOrReadOnly)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """Вьюсет для работы с моделью Comment."""
+    serializer_class = CommentSerializer
+    permission_classes = (OwnerOrReadOnly, IsAuthenticatedOrReadOnly)
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, pk=self.kwargs['post_id'])
+        return post.comments.all()
+
+    def perform_create(self, serializer):
+        post_id = self.kwargs['post_id']
+        post = get_object_or_404(Post, pk=post_id)
+        serializer.save(author=self.request.user, post=post)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    """Вьюсет для работы с моделью Group."""
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = (GroupOnlyGet,)
