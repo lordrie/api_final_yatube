49ad4ec3f4a056456e2eedcc4dedc2f1fed5c604
Никита Прокофьев <aeriexye@gmail.com>
2023-09-07 15:11:23 +0300

    add jwt

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,9 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
+    path('v1/', include(router.urls)),
+    path('v1/', include('djoser.urls.jwt')),
 ]
