99254a8521fa05619b92b892c8287b157d133502
Никита Прокофьев <aeriexye@gmail.com>
2023-09-11 09:07:19 +0300

    add urls.follow & followviewset

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,14 +1,13 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import CommentViewSet, GroupViewSet, PostViewSet
+from .views import CommentViewSet, GroupViewSet, PostViewSet, FollowViewSet
 
 router = routers.DefaultRouter()
 router.register('posts', PostViewSet, basename='posts')
 router.register('groups', GroupViewSet, basename='groups')
 router.register(
-    r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comments'
-)
-
+    r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comments')
+router.register('follow', FollowViewSet, basename='follow')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,10 +1,13 @@
 from posts.models import Post, Group
-from .serializers import PostSerializer, GroupSerializer, CommentSerializer
+from .serializers import (PostSerializer, GroupSerializer,
+                          CommentSerializer, FollowSerializer)
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import (IsAuthenticatedOrReadOnly,
+                                        IsAuthenticated)
 from django.shortcuts import get_object_or_404
 from .permissions import OwnerOrReadOnly, GroupOnlyGet
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.filters import SearchFilter
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -38,3 +41,16 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = (GroupOnlyGet,)
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    serializer_class = FollowSerializer
+    permission_classes = (IsAuthenticated,)
+    filter_backends = (SearchFilter,)
+    search_fields = ('following__username',)
+
+    def get_queryset(self):
+        return self.request.user.follower.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
