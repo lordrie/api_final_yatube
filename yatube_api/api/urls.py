b321c11ffcee425cc4397198186f39dd90ca7385
nickpydj <aeriexye@gmail.com>
2023-09-13 13:39:51 +0300

    fix jwt

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
 from rest_framework import routers
 from .views import CommentViewSet, GroupViewSet, PostViewSet, FollowViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView, TokenRefreshView, TokenVerifyView)
 
 router = routers.DefaultRouter()
 router.register('posts', PostViewSet, basename='posts')
@@ -9,7 +11,13 @@ router.register(
     r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comments')
 router.register('follow', FollowViewSet, basename='follow')
 
+jwt_patterns = [
+    path('create/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('verify/', TokenVerifyView.as_view(), name='token_verify'),
+]
+
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('v1/', include('djoser.urls.jwt')),
+    path('v1/jwt/', include(jwt_patterns)),
 ]
